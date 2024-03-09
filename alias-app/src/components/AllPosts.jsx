fbcaf2647a6b69bde489d072a26c7950a104b8be
MiaDHayes <miadeyon@outlook.com>
2024-03-09 15:58:04 -0600

    AllPosts is sorted in order from most recent to oldest and AddPost has form and chips (tags)

--- alias-app/src/components/AllPosts.jsx
+++ alias-app/src/components/AllPosts.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react'
 import AddPost from '../helpers/AddPost'
 
 function AllPosts() {
+	const [posts, setPosts] = useState([])
+	const [isLoading, setIsLoading] = useState()
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			try {
+				const response = await fetch ('api/posts')
+				const data = await response.json()
+				//sort
+				const sortedPosts = data.posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+				setPosts(data.posts)
+			} catch (error) {
+				console.error('Error fetching posts:', error)
+			}
+		}
+		fetchPosts()
+	}, [])
+	
 	return (
-		<>
-		<AddPost/>
+		<div>
+			<AddPost/>
+			<div className='all-posts'>
+				{posts.map(post => (
+					<div key={post.id}>
+						<h3>{post.title}</h3>
+						<p>{post.content}</p>
+					</div>
+				))}
+			</div>
 		{/* Dedicated page to display all the posts, in descending order (latest to oldest) */}
-		</>
+		</div>
 	)
 }
 export default AllPosts
--- alias-app/src/helpers/AddPost.jsx
+++ alias-app/src/helpers/AddPost.jsx
@@ -1,8 +1,47 @@
-function AddPost() {
+import { useState } from "react"
+
+
+function AddPost({ onSubmit }) {
+
+	const initialState = {
+		title: '',
+		alias: '',
+		content: '',
+//chips instead of tags (it's the same thing but this is how we can make it buttons)
+		chips: [],
+		createdAt: new Date()
+	}
+	const [postFormState, setPostFormState] = useState(initialState)
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		console.log(postFormState)
+		setPostFormState(initialState)
+	}
+
+	const handleChange = (event) => {
+		setPostFormState({...postFormState, [event.target.id]: event.target.value})
+	}
+
+	const handleChipAddition = (chip) => {
+		setPostFormState({...postFormState, chips: [...postFormState.chips, chip]})
+	}
+
 	return (
-		<>
+		<div>
+			<h2>Create a Post</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="title">Title: </label>
+				<input type="text" id="title" onChange={handleChange} value={postFormState.title}/>
+				<label htmlFor="alias">Alias: </label>
+				<input type="text" id="alias" onChange={handleChange} value={postFormState.alias}/>
+				<label htmlFor="content">What's on your mind?</label>
+				<textarea id="content" cols="30" rows="30" onChange={handleChange} value={postFormState.content}/>
+				{/* <ChipInput onAddChip={handleChipAddition}/> */}
+				<button type="submit"> Post </button>
+			</form>
 		{/* Component that is nested into Home, and All Posts */}
-		</>
+		</div>
 	)
 }
 export default AddPost
