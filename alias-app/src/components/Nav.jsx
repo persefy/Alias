f4aca6cd4c8d1b659c21f05fdd2ddbf30fa685db
Felix Man <felixwhman@gmail.com>
2024-03-09 19:09:05 -0500

    Created Links for Nav

--- alias-app/src/components/Nav.jsx
+++ alias-app/src/components/Nav.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom'
+
 function Nav() {
 	return (
-		<nav>
-			{/* links to pages:
-			
-			- trending
-			- ...
-			*/}
+		<nav className="navbar">
+			<h4>Alias</h4>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to="/trending">Trending</Link>
+				<Link to="/tags/:id">Tags</Link>
+			</div>
 		</nav>
-
 	)
 }
 export default Nav
