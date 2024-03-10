b33ce00cc084ad39facffe5b926635988f6cc567
Felix Man <felixwhman@gmail.com>
2024-03-09 21:48:44 -0500

    Updated Home component are we adding a button for addpost or new page link? are we using useContext to pass logged in information through the website?

--- alias-app/src/components/Nav.jsx
+++ alias-app/src/components/Nav.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 function Nav() {
 	return (
 		<nav className="navbar">
-			<h4>Alias</h4>
 			<div>
 				<Link to="/">Home</Link>
 				<Link to="/trending">Trending</Link>
