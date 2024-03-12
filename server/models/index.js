e530e9b5a15926136437a79d27d03f55e5d6041b
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-03-12 11:31:10 -0400

    Made changes to remove tag entity from all backend files. Updated posts to take tags as booleans

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose')
 const ContactMsgSchema = require('./contactMsg')
 const PostSchema = require('./post')
-const TagSchema = require('./tag')
 const UserSchema = require('./user')
 
 const ContactMsg = mongoose.model('ContactMsg', ContactMsgSchema)
 const Post = mongoose.model('Post', PostSchema)
-const Tag = mongoose.model('Tag', TagSchema)
 const User = mongoose.model('User', UserSchema)
 
 
 module.exports = {
     ContactMsg,
     Post,
-    Tag,
     User
 }
