c1f0501edcb6f57116943f1800b7feebb90d0f65
MiaDHayes <miadeyon@outlook.com>
2024-03-12 10:39:05 -0500

    HomePost and Reactions updated

--- alias-app/src/helpers/HomePosts.jsx
+++ alias-app/src/helpers/HomePosts.jsx
@@ -1,11 +1,23 @@
-import AllPosts from '../components/AllPosts'
 
-function HomePosts() {
+function HomePosts({ posts, startDate, endDate}) {
+	const filteredPosts = posts.filter(post => {
+		const postDate = new Date(post.createdAt)
+		return postDate >= startDate && postDate <= endDate
+	})
+
+
 	return (
 		<div>
-			<AllPosts />
-		{/* Component that is nested into Home */}
+			<ul>
+				{filteredPosts.map(post => (
+					<li key={post.id}>
+						<h3>{post.title}</h3>
+						<p>{post.content}</p>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
 export default HomePosts
+/* Component that is nested into Home */
