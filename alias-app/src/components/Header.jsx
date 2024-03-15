34c8cd613f037f8b4e747956ffa5271a887f0edd
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-03-15 05:22:35 -0400

    updated home, nav, andd header with some of the needed changes

--- alias-app/src/components/Header.jsx
+++ alias-app/src/components/Header.jsx
@@ -1,8 +1,45 @@
 import Nav from './Nav'
+import { useContext, useEffect} from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import { UserContext } from '../UserContext'
+
 function Header() {
-	return (
+	let navigate = useNavigate()
+	
+	const { isLoggedIn, setIsLoggedIn, userInfo, setUserInfo} = useContext(UserContext)
+	//temp code below for testing
+	useEffect(()=> {
+		setIsLoggedIn(true)
+
+	},[])
+	//temp code above for testing
+
+	return !isLoggedIn ? (
 		<header>
-			<h1>Alias</h1>
+			<h1 onClick={()=> {navigate('/')}}>Alias</h1>
+			<div className='userMgmtNav'>
+				<div>
+					{/* <Link to="/login" className='logInLink'>Log In</Link> */}
+					<Link to="/create" className='createLoginLink'>Create Account</Link>
+				</div>
+			</div>
+			<Nav/>
+		</header>
+	): (
+		<header>
+			<h1 onClick={()=> {navigate('/')}}>Alias</h1>
+			<div className='userMgmtNav'>
+				<div>
+					<Link to="/user/:username" className='userPgLink'>User</Link>
+					<button className='logOutBtn' onClick={()=> {
+						console.log('clicked on logout!')
+						navigate('/')
+						setIsLoggedIn(false)
+						setUserInfo({})
+
+						}}>Log Out</button>
+				</div>
+			</div>
 			<Nav/>
 		</header>
 	)
